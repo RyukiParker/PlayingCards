75ac9f23ff782d457690945652bb9c8c5afd7884
iparamosh <91302209+iparamosh@users.noreply.github.com>
2021-09-29 09:15:44 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,20 +19,8 @@ class Main {
       }
       deck[i] = new Card(g, (i % 13) + 1, suit);
     }
-    System.out.println(Arrays.asList(deck));
 
-    // Draw cards on screen
-    for(int h=0;h<6;h++) {
-      deck[h].draw(h*70, 10, 60);
-    }
-    for(int s=13;s<19;s++) {
-      deck[s].draw((s-13)*70, 110, 60);
-    }
-    for(int d=26;d<32;d++) {
-      deck[d].draw((d-26)*70, 210, 60);
-    }
-    for(int c=39;c<45;c++) {
-      deck[c].draw((c-39)*70, 310, 60);
-    }
+    deck[0] = new Card(g, 8, 3);
+    deck[0].draw(20, 50);
   }
 }
